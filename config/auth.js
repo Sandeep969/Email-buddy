79f2920ffa7a206f8d1955563b6ae2843d7a11cf
sandeep969 <sunnyandsandeep1996@gmail.com>
2017-03-24 03:59:46 +0530

    json

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,6 @@ module.exports = {
 	'googleAuth' : {
 		'clientID': '221154299124-armq8gqi869ll48j6o09ru9srta9khjs.apps.googleusercontent.com',
 		'clientSecret': 'aUSy_6aRlg7D7Y7P4Xa6syb9',
-		'callbackURL': 'https://murmuring-bayou-26299.herokuapp.com//auth/google/callback'
+		'callbackURL': 'https://murmuring-bayou-26299.herokuapp.com/auth/google/callback'
 	}
 }
